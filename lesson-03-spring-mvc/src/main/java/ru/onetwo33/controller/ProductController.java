3a314d4cc335370c62afe42300e10ba238e28b8a
OneTwo33 <alex_tsai@mail.ru>
2021-06-25 14:23:14 +0300

    homework 3

---
+++ lesson-03-spring-mvc/src/main/java/ru/onetwo33/controller/ProductController.java
@@ -0,0 +1,58 @@
+package ru.onetwo33.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.onetwo33.persist.Product;
+import ru.onetwo33.persist.ProductRepository;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public String listPage(Model model) {
+        logger.info("Product list page requested");
+
+        model.addAttribute("products", productRepository.findAll());
+        return "products";
+    }
+
+    @GetMapping("/new")
+    public String newProductForm(Model model) {
+        logger.info("New product page requested");
+
+        model.addAttribute("product", new Product());
+        return "product_form";
+    }
+
+    @GetMapping("/{id}")
+    public String editProduct(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("product", productRepository.findById(id));
+
+        return "product_form";
+    }
+
+    @PostMapping
+    public String update(Product product) {
+        logger.info("Saving product");
+
+        productRepository.save(product);
+        return "redirect:/product";
+    }
+}
