ba6cecadfb1158a7d6b6fa0ba7cd215b752d1379
OneTwo33 <alex_tsai@mail.ru>
2021-07-02 14:12:49 +0300

    homework 5 ProductDao

---
+++ lesson-05-hibernate/src/main/java/ru/onetwo33/entity/Product.java
@@ -0,0 +1,60 @@
+package ru.onetwo33.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 512, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Float cost;
+
+    public Product() {
+    }
+
+    public Product(Long id, String title, Float cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getCost() {
+        return cost;
+    }
+
+    public void setCost(Float cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
